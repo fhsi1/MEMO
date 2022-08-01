d231e3ab41b845d45aec590f89f073f6c2e6c4fa
Yujean Cho <eugenie8@kakao.com>
2022-08-01 16:45:16 +0900

    [UPDATE] Add TableViewController Class

---
+++ Memo/MemoListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MemoListTableViewController.h
+//  Memo
+//
+//  Created by Yujean Cho on 2022/08/01.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MemoListTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
