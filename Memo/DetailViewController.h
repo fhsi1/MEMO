ad673fb2b2684db419a1d072c1822b4422b9a27f
Yujean Cho <eugenie8@kakao.com>
2022-08-02 14:18:12 +0900

    [UPDATE] Add ViewController

---
+++ Memo/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  Memo
+//
+//  Created by Yujean Cho on 2022/08/02.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
